a00072a4bd6aa6b91cb25b35ebd8c42a8fc70c52
Simon Temple <simonctemple@live.com>
2016-04-16 19:29:31 -0700

    impliment crud functionality

--- src/ToDo/Controllers/ItemsController.cs
+++ src/ToDo/Controllers/ItemsController.cs
@@ -1,15 +1,69 @@
 ﻿using Microsoft.AspNet.Mvc;
 using System.Linq;
 using ToDo.Models;
+using Microsoft.Data.Entity;
 
 namespace ToDo.Controllers
 {
     public class ItemsController : Controller
     {
+        // index
         private ToDoContext db = new ToDoContext();
         public IActionResult Index()
         {
             return View(db.Items.ToList());
+        }
+
+        // details
+        public IActionResult Details(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(thisItem);
+        }
+
+        // create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            db.Items.Add(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //edit
+        public IActionResult Edit(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Item item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //delete
+        public IActionResult Delete(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            db.Items.Remove(thisItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
